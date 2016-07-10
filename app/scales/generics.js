25a144cb6965252cef7c7f0072c614f285ce6215
nyxsys1 <mbarbera@udel.edu>
2016-07-10 21:28:18 +0000

    added named groups to regexA

--- app/scales/generics.js
+++ app/scales/generics.js
@@ -1,5 +1,10 @@
-var generics = {};
-generics.generic_parse = /([iI][Vv]|[Vv]?[Ii]{0,3})(aug|dim|ma|m)?(6|7|9|11|13)?(?:(b|#)(5|7|9|11|13))?/;
+var named = require('named-regexp').named;
+
+//var generics = {};
+
+var re = named(/(:<tone1>b|#)?(:<roman>[iI][Vv]|[Vv]?[Ii]{0,3})(:<quality>aug|dim|ma|m)?(:<tension1>6|7|9|11|13)?(?:(:<tone2>b|#)(:<tension2>5|7|9|11|13))?/);
+
+var match = re.exec("IIma7");
 
 
 generics.verification = function(){
